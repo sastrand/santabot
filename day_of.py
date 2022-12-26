d4e10db9c96f5a7caadc75bfb92c56e7146072e1
Sascha Strand <12968575+sastrand@users.noreply.github.com>
2022-12-26 08:00:01 -0800

    Update day_of.py

--- day_of.py
+++ day_of.py
@@ -1,4 +1,8 @@
-# Top Cell
+# 2022-12-26
+# Run this in a Jupyter Notebook where just the second cell can be run iteratively 
+# The `values` list is copy-pasted from the santabot script. Make sure it's up-to-date.
+
+# First Cell
 
 import random
 
@@ -20,11 +24,10 @@ values = {
 }
 
 family = [v[0] for v in values]
-
 random.shuffle(family)
 
 
-# Next cell
+# Second Cell
 
 print(family.pop())
 print(f'remaining {len(family)} of {len(values)}')
